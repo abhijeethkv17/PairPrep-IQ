91c1175413551bf5dc553a2b7906ee48f35bb958
Abhijeeth K V <abhijeethkv17@gmail.com>
2026-08-06 08:32:48 +0530

    04: Syncing Clerk Auth with Database

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,4 +5,11 @@ export const ENV = {
   PORT: process.env.PORT,
   MONGO_DB_URI: process.env.MONGO_DB_URI,
   NODE_ENV: process.env.NODE_ENV,
+  // INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+  // INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_API_SECRET: process.env.STREAM_API_SECRET,
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  CLIENT_URL: process.env.CLIENT_URL,
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,35 +1,48 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
+// middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 app.get("/health", (req, res) => {
-  res.status(200).json({ message: "API is up and running!" });
+  res.status(200).json({ msg: "api is up and running" });
 });
 
 app.get("/books", (req, res) => {
-  res.status(200).json({ message: "Books API is up and running!" });
+  res.status(200).json({ msg: "this is the books endpoint" });
 });
 
+// make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("/{*any}", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => {
-      console.log(`Server is running on port: ${ENV.PORT}`);
-    });
+    app.listen(ENV.PORT, () =>
+      console.log("Server is running on port: ", ENV.PORT),
+    );
   } catch (error) {
-    console.error("Error starting server: ", error);
+    console.error("Error starting the server: ", error);
   }
 };
 
